3958c897343e11f996597c295c3a65d23c157144
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-26 11:03:53 +0530

    product class

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../common/product';
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
 }
