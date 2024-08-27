53e931114a58949c86ea086da65678f85218c76d
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-27 14:13:58 +0530

    search added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ProductCategoryMenuComponent } from './components/product-category-menu
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 const routes: Routes = [
   {path:'products/:id', component: ProductDetailsComponent},
   {path:'search/:keyword', component: ProductListComponent},
@@ -23,7 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, ProductDetailsComponent],
-  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule, NgbModule],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
