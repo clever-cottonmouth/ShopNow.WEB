5363afb47407bdad0fab9024f8c1595068684b72
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-27 12:13:47 +0530

    product details

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -21,11 +21,17 @@ interface GetResponseProductCategory {
   providedIn: 'root'
 })
 export class ProductService {
+
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
+
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
