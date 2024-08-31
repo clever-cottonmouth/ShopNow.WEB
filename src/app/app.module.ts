072eb11c9d8cc8c9c377cfb82e44710e105eacd6
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-31 10:56:35 +0530

    checkout ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,11 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
+  {path:'checkout', component: CheckoutComponent},
   {path:'cart-details', component: CartDetailsComponent},
   {path:'products/:id', component: ProductDetailsComponent},
   {path:'search/:keyword', component: ProductListComponent},
@@ -27,8 +30,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, ProductDetailsComponent, CartStatusComponent, CartDetailsComponent],
-  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule, NgbModule],
+  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, ProductDetailsComponent, CartStatusComponent, CartDetailsComponent, CheckoutComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule, NgbModule, ReactiveFormsModule],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
